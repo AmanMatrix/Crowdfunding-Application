9749f8fcd97d8d7db627b52806c54e00a8baadd9
AmanMatrix <itsamansingla11@gmail.com>
2022-04-04 14:32:55 +0530

    minor formatting

--- Backend/index.js
+++ Backend/index.js
@@ -1,20 +1,20 @@
-const connectToMongo = require('./db');
+const connectToMongo = require("./db");
 
 connectToMongo();
 
-const express = require('express')
-const app = express()
-const port = 5000
+const express = require("express");
+const app = express();
+const port = 5000;
 
 app.use(express.json());
 
-//Routes available
-app.use('/api/auth/newuser',require('./routes/auth'));
+//Available Routes
+app.use("/api/auth/newuser", require("./routes/auth"));
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
