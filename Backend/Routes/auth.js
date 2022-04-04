a21359d8237f11fe53109d67966e919d24b63ea6
Akshit <akshitmaheshwari15102001@gmail.com>
2022-04-04 22:56:46 +0530

    added username in schema

--- Backend/Routes/auth.js
+++ Backend/Routes/auth.js
@@ -18,32 +18,42 @@ router.post(
   ],
   async (req, res) => {
     //if errors occur, returns bad request
-    const errors = validationResult(req);
-
-
-    try {
-      let user = await User.findOne({ email: req.body.email });
-      if (user) {
-        res.status(400).json({ error: "Email already in use" });
-      } else {
-        //   Creating hash from password and adding salt
-        const salt = await bcrypt.genSaltSync(10);
-        const hash = bcrypt.hashSync(req.body.password, salt);
-
-        //   Creating a new user
-        user = await User.create({
-          name: req.body.name,
-          email: req.body.email,
-          password: hash,
-        });
-        // Generating a JWT
-        const authtoken = jwt.sign({ id: user.id }, jwt_secret);
-        res.json({ authtoken });
-      }
-    } catch {
-      return res.status(500).json({ error: "Some error occured" });
+    const errors = await validationResult(req);
+    if(!errors.isEmpty())
+    {
+        return res.status(400).json({errors: errors.array()});
     }
-  }
+    else
+    {
+        try {
+        let user1 = await User.findOne({ email: req.body.email });
+        let user2 = await User.findOne({ username: req.body.username });
+        if (user1 || user2) {
+            if(user1)
+                res.status(400).json({ error: "Email already in use" });
+            else
+                res.status(400).json({ error: "Username already in use" });
+        } else {
+            //   Creating hash from password and adding salt
+            const salt = await bcrypt.genSaltSync(10);
+            const hash = bcrypt.hashSync(req.body.password, salt);
+
+            //   Creating a new user
+            let user = await User.create({
+            name: req.body.name,
+            email: req.body.email,
+            password: hash,
+            username : req.body.username
+            });
+            // Generating a JWT
+            const authtoken = jwt.sign({ id: user.id }, jwt_secret);
+            res.json({ authtoken });
+        }
+        } catch {
+        return res.status(500).json({ error: "Some error occured" });
+        }
+    }
+    }
 );
 
 module.exports = router;
